8720a4fcebe65065e1b485cdfc60e6a699879a2f
Rishab Goyal <rishabgoyal432@gmail.com>
2021-02-14 19:57:00 +0530

    Intial Setup

---
+++ src/components/EditorComponent.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Editor extends Component {
+    constructor(props) { 
+        super(props);
+    }
+    render() { 
+        return ( 
+            
+            <h4>Hey i am a code editor</h4>
+        );
+    }
+}
+
+export default Editor;
---
+++ src/components/ProblemComponent.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Problem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <h4>Hey i am a Problem</h4>
+        );
+    }
+}
+
+export default Problem;
