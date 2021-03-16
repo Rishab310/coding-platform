33a5478bebf8b57f07f4f1c08cfdf4a88ee06f7d
Rishab Goyal <rishabgoyal432@gmail.com>
2021-03-16 18:01:51 +0530

    new dependencies

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import Editor from "./components/codeEditor/EditorComponent";
-import AceExample from "./components/codeEditor/AceComponent"; 
-import Problem from "./components/codeEditor/ProblemComponent";
+import Main from './components/codeEditor/EditorMain';
+import LoginPage from './components/loginPage';
 class App extends Component {
     render() {
         return (
-            <div className="container m-5">
-                <div className="row">
-                    <div className="col-12">
-                        <h3>Coding Platform.</h3>
-                    </div>
-                </div>
-                <Editor />
-                <AceExample/>
-            </div>
+            <>
+                <LoginPage/>
+            </>
         );
     }
 }
